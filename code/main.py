d5146ddad34ac2c7a0ab5ab6fd27afc1315a1417
Aleksandr Korzh <korzhstud@gmail.com>
2020-02-14 22:50:25 +0300

    Добавлен список дней. Список месяцев теперь передается параметром. Добавлены строки док-я функций

--- code/main.py
+++ code/main.py
@@ -1,17 +1,20 @@
 # Тесты
 test = []
+month = {'мар': 1, 'апр': 2, 'мая': 3, 'июн': 4, 'июл': 5, 'авг': 6, 'сен': 7, 'окт': 8, 'ноя': 9, 'дек': 10,
+             'янв': 11, 'фев': 12}
+days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
 
 # Объявление функций
-def get_data(string):
+def get_data(string, month):
     '''Функция преобразовывает строку пользователся в набор числовых
     значений для дальнейшей работы с ними.
 
     :param string typed string:
+    :param month typed dict:
     :return day, month, century, year typed tuple:
 
     '''
-    month = {'мар': 1, 'апр': 2, 'мая': 3, 'июн': 4, 'июл': 5, 'авг': 6, 'сен': 7, 'окт': 8, 'ноя': 9, 'дек': 10,
-             'янв': 11, 'фев': 12}
+
     data = string.split()
     d = int(data[0])
     m = month[data[1][0:3]]
@@ -48,9 +51,9 @@ while choice != 2:
 
     date = input('Введите дату >>> ')
     print('-' * 23)
-    data = get_data(date)
+    data = get_data(date, month)
     res = get_answer(*data)
-    print(res)
+    print(days[res])
     #prettify()
 
     try:

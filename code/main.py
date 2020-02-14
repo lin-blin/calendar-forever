9bcdf048a14aecc5f5cb47f7a6cb1a5af44cca37
Aleksandr Korzh <korzhstud@gmail.com>
2020-02-14 21:50:25 +0300

    Пофиксил вычислиельную функцию

--- code/main.py
+++ code/main.py
@@ -1,7 +1,8 @@
 import math
 
 string = '13 февраля 2020'
-#string = '30 октября 2019'
+string = '30 октября 2019'
+string = '16 февраля 2020'
 
 def get_data(string):
     month = {'мар': 1, 'апр': 2, 'мая': 3, 'июн': 4, 'июл': 5, 'авг': 6, 'сен': 7, 'окт': 8, 'ноя': 9, 'дек': 10,
@@ -10,15 +11,19 @@ def get_data(string):
     d = int(data[0])
     m = month[data[1][0:3]]
     y = int(data[2]) % 100
+    if m > 10:
+        y -= 1
     c = int(data[2]) // 100
-    return (d, m, c, y)
+    return d, m, c, y
 
 data = get_data(string)
 print(data)
 
 def get_answer(d, m, c, y):
-    answer = (d + int((13*m-1)/5) + y + int(y/4) + int(c/4) -3*c +777) % 7
+    answer = (d + int((13*m-1)/5) + y + int(y/4) -2*c + int(c/4) ) % 7
     return answer
 
+
 res = get_answer(*data)
+
 print(res)
